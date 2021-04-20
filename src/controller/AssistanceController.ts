3a819f4d03b1e8995991c7a66794cd730e282b94
João Victor Santos <profissional.joaovss@gmail.com>
2021-04-19 21:02:42 -0300

    fix: try-catch

--- src/controller/AssistanceController.ts
+++ src/controller/AssistanceController.ts
@@ -8,40 +8,87 @@ export class AssistanceController {
     private assistanceRepository = getRepository(Assistance);
 
     async all(request: Request, response: Response, next: NextFunction) {
-        const builder = new FindOptionBuilder(request.query);
-        // Set allowed fields
-        builder.setAllowedFields(['limit', 'pagination', 'page', 'order', 'select']); // commons
-        builder.setAllowedFields(['id', 'pjo_unidadeid', 'pjo_blocoid', 'actualstart', 'actualend', 'subject', 'pjo_tipodeatividade', 'pjo_empreendimentoid']); // table fields
+        try {
+            const builder = new FindOptionBuilder(request.query);
+            // Set allowed fields
+            builder.setAllowedFields(['limit', 'pagination', 'page', 'order', 'select']); // commons
+            builder.setAllowedFields(['id', 'pjo_unidadeid', 'pjo_blocoid', 'actualstart', 'actualend', 'subject', 'pjo_tipodeatividade', 'pjo_empreendimentoid']); // table fields
 
-        // Build query
-        const builtQuery = builder.build();
-
-        return this.assistanceRepository.find(builtQuery);
+            // Build query
+            const builtQuery = builder.build();
+            const all = await this.assistanceRepository.find(builtQuery);
+            return response.status(200).json({
+                message: "find all operation success.",
+                data: all,
+            });
+        } catch (error) {
+            return response.status(400).json({
+                message: "find all operation failed, try again.",
+                info: error,
+            });
+        }
     }
 
     async one(request: Request, response: Response, next: NextFunction) {
-        const result = await this.assistanceRepository.findOne(request.params.id)
-        if (result) {
-            return response.status(200).json(result);
+        const { id } = request.params;
+        try {
+            const one = await this.assistanceRepository.findOne(id);
+            return response.status(200).json({
+                message: "find operation success.",
+                data: one,
+            });
+        } catch (error) {
+            return response.status(400).json({
+                message: "find operation failed, try agaain.",
+                info: error,
+            });
         }
-        return response.status(404);
     }
 
     async save(request: Request, response: Response, next: NextFunction) {
-        const result = await this.assistanceRepository.save(request.body)
-        if (result) {
-            return response.status(200).json(result.id);
+        try {
+            const created = await this.assistanceRepository.save(request.body);
+            response.status(201).json({
+                message: "create operation success.",
+                data: created,
+            });
+        } catch (error) {
+            response.status(400).json({
+                message: "create operation failed, try again.",
+                info: error,
+            });
         }
-        return response.status(500);
     }
 
     async remove(request: Request, response: Response, next: NextFunction) {
-        let toRemove = await this.assistanceRepository.findOne(request.params.id);
-        if (toRemove.id) {
-            await this.assistanceRepository.remove(toRemove);
-            return response.status(200)
+        const { id } = request.params;
+        try {
+            await this.assistanceRepository.delete(id);
+            return response.sendStatus(200).json({ message: "delete operation success." });
+        } catch (error) {
+            return response.status(400).json({
+                message: "delete operation failed, try again.",
+                info: error,
+            });
         }
-        return response.status(404)
     }
 
+    async update(request: Request, response: Response, next: NextFunction) {
+        const { id } = request.params;
+        try {
+            const updated = await this.assistanceRepository.update(
+                id,
+                request.body
+            );
+            return response.status(200).json({
+                message: "update operation success.",
+                data: updated,
+            });
+        } catch (error) {
+            return response.status(400).json({
+                message: "update operation failed, try again.",
+                info: error,
+            });
+        }
+    }
 }
--- src/controller/OccurrenceController.ts
+++ src/controller/OccurrenceController.ts
@@ -8,32 +8,87 @@ export class OccurrenceController {
     private occurrenceRepository = getRepository(Occurrence);
 
     async all(request: Request, response: Response, next: NextFunction) {
-        const builder = new FindOptionBuilder(request.query);
-        // Set allowed fields
-        builder.setAllowedFields(['limit', 'pagination', 'page', 'order', 'select']); // commons
-        builder.setAllowedFields(['id', 'ticketnumber', 'pjo_clientedaunidade', 'pjo_empreendimentoid', 'pjo_bloco', 'pjo_unidade', 'pjo_bandeira', 'description']); // table fields
+        try {
+            const builder = new FindOptionBuilder(request.query);
+            // Set allowed fields
+            builder.setAllowedFields(['limit', 'pagination', 'page', 'order', 'select']); // commons
+            builder.setAllowedFields(['id', 'ticketnumber', 'pjo_clientedaunidade', 'pjo_empreendimentoid', 'pjo_bloco', 'pjo_unidade', 'pjo_bandeira', 'description']); // table fields
 
-        // Build query
-        const builtQuery = builder.build();
-
-        return this.occurrenceRepository.find(builtQuery);
+            // Build query
+            const builtQuery = builder.build();
+            const all = await this.occurrenceRepository.find(builtQuery);
+            return response.status(200).json({
+                message: "find all operation success.",
+                data: all,
+            });
+        } catch (error) {
+            return response.status(400).json({
+                message: "find all operation failed, try again.",
+                info: error,
+            });
+        }
     }
 
     async one(request: Request, response: Response, next: NextFunction) {
-        return this.occurrenceRepository.findOne(request.params.id);
+        const { id } = request.params;
+        try {
+            const one = await this.occurrenceRepository.findOne(id);
+            return response.status(200).json({
+                message: "find operation success.",
+                data: one,
+            });
+        } catch (error) {
+            return response.status(400).json({
+                message: "find operation failed, try agaain.",
+                info: error,
+            });
+        }
     }
 
     async save(request: Request, response: Response, next: NextFunction) {
-        return this.occurrenceRepository.save(request.body);
+        try {
+            const created = await this.occurrenceRepository.save(request.body);
+            response.status(201).json({
+                message: "create operation success.",
+                data: created,
+            });
+        } catch (error) {
+            response.status(400).json({
+                message: "create operation failed, try again.",
+                info: error,
+            });
+        }
     }
 
     async remove(request: Request, response: Response, next: NextFunction) {
-        let toRemove = await this.occurrenceRepository.findOne(request.params.id);
-        if (toRemove.id) {
-            await this.occurrenceRepository.remove(toRemove);
-            return response.status(200)
+        const { id } = request.params;
+        try {
+            await this.occurrenceRepository.delete(id);
+            return response.sendStatus(200).json({ message: "delete operation success." });
+        } catch (error) {
+            return response.status(400).json({
+                message: "delete operation failed, try again.",
+                info: error,
+            });
         }
-        return response.status(404)
     }
 
+    async update(request: Request, response: Response, next: NextFunction) {
+        const { id } = request.params;
+        try {
+            const updated = await this.occurrenceRepository.update(
+                id,
+                request.body
+            );
+            return response.status(200).json({
+                message: "update operation success.",
+                data: updated,
+            });
+        } catch (error) {
+            return response.status(400).json({
+                message: "update operation failed, try again.",
+                info: error,
+            });
+        }
+    }
 }
